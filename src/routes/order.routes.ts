6da8c29d940aff431cdbb2eb77702e8a869cc500
Harsh Baldaniya <baldaniyaharsh5@gmail.com>
2025-01-20 16:50:08 +0530

    change routes

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,21 +1,19 @@
 import { Router } from "express";
 import { AwilixContainer } from "awilix";
 import OrderController from "../controllers/order.controller";
-import { defaultRole, restrictToRoles, verifyToken } from "../middlewares/authMiddleware";
+import { defaultRole } from "../middlewares/authMiddleware";
 import { validateCreateOrder, validateUpdateOrder } from "../validators/order.validator";
 
 const orderRoutes = (container: AwilixContainer): Router => {
     const router = Router();
     const orderController = container.resolve("orderController") as OrderController;
-
-
-    router.post("/create", verifyToken, restrictToRoles(["test", "admin"]), validateCreateOrder, orderController.createOrder);
-    router.patch("/update", verifyToken, restrictToRoles(["test", "admin"]), validateUpdateOrder, orderController.updateOrder);
+    
+    router.post("/create", validateCreateOrder, ...defaultRole(orderController.createOrder, ["school", "test"]));
+    router.patch("/update", validateUpdateOrder, ...defaultRole(orderController.updateOrder, ["test"]));
     router.get("/list", ...defaultRole(orderController.listOrders, ["school", "test"]));
-    router.patch("/archive-order", verifyToken, restrictToRoles(["test", "admin"]), orderController.archiveOrder);
-    router.delete("/remove-order", verifyToken, restrictToRoles(["test", "admin"]), orderController.removeOrder);
+    router.patch("/archive-order", ...defaultRole(orderController.archiveOrder, ["test"]));
+    router.delete("/remove-order", ...defaultRole(orderController.removeOrder, ["test"]));
 
-  
     return router;
   };
 
